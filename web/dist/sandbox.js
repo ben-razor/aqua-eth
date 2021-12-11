a6fd08fe61027e430cf228593f6e68ad47f446b8
Ben Razor <chrisbenwar1@googlemail.com>
2021-12-11 16:29:37 +0000

    Create build

--- web/dist/sandbox.js
+++ web/dist/sandbox.js
@@ -1,7 +1,7 @@
 /*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * ATTENTION: An "eval-source-map" devtool has been used.
  * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
  * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
@@ -15,7 +15,7 @@
   \************************/
 /***/ (() => {
 
-eval("document.getElementById('root').innerHTML = '<h1>It sandboxes</h1>';\n\n//# sourceURL=webpack://aqua-explore-react-new/./src/sandbox.js?");
+eval("document.getElementById('root').innerHTML = '<h1>It sandboxes</h1>';//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9hcXVhLWV4cGxvcmUtcmVhY3QtbmV3Ly4vc3JjL3NhbmRib3guanM/OTQzOSJdLCJuYW1lcyI6WyJkb2N1bWVudCIsImdldEVsZW1lbnRCeUlkIiwiaW5uZXJIVE1MIl0sIm1hcHBpbmdzIjoiQUFBQUEsUUFBUSxDQUFDQyxjQUFULENBQXdCLE1BQXhCLEVBQWdDQyxTQUFoQyxHQUE0Qyx1QkFBNUMiLCJzb3VyY2VzQ29udGVudCI6WyJkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgncm9vdCcpLmlubmVySFRNTCA9ICc8aDE+SXQgc2FuZGJveGVzPC9oMT4nOyJdLCJmaWxlIjoiLi9zcmMvc2FuZGJveC5qcy5qcyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/sandbox.js\n");
 
 /***/ })
 
@@ -24,7 +24,7 @@ eval("document.getElementById('root').innerHTML = '<h1>It sandboxes</h1>';\n\n//
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
 /******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./src/sandbox.js"]();
 /******/ 	
